534409a1beb96c38d4443f4d2c14f4b8f072d8c4
Paweł Brzozowski <pawel@brzoza.net>
2024-02-09 14:27:10 +0100

    store refactoring

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,15 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { rootReducer } from "./reducer";
+// import { createStore } from "redux";
+// import { devToolsEnhancer } from "@redux-devtools/extension";
+// import { rootReducer } from "./reducer";
 
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
 
 // NOTE: Redux Toolkit
+
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "../redux/reducer";
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
